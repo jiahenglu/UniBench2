161c87e2a9be02f38069812bcb37a25df126bd7b
Paddy Xu <xupaddy@gmail.com>
2020-05-05 17:09:13 +0300

    dicLocations.txt to RDF

--- Preprocessing/src/main/java/Preprocessing/TagIdGenerator.java
+++ Preprocessing/src/main/java/Preprocessing/TagIdGenerator.java
@@ -7,10 +7,8 @@ import org.eclipse.rdf4j.model.util.ModelBuilder;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TagIdGenerator {
@@ -20,6 +18,7 @@ public class TagIdGenerator {
         var parent_classes = Rio.parse(TagRankingGenerator.class.getResourceAsStream("/product_type_tree.ttl"), "", RDFFormat.TURTLE);
 
         var tags_id = assignIds(tags.subjects().stream().map(Resource::stringValue).collect(Collectors.toSet()));
+        var companies_id = assignIds(tags.objects().stream().map(Value::stringValue).collect(Collectors.toSet()));
 
         var all_classes = new HashSet<>(classes.objects());
         all_classes.addAll(parent_classes.subjects());
@@ -28,17 +27,50 @@ public class TagIdGenerator {
 
         new File("./output/").mkdir();
         Rio.write(tags_id, new FileWriter("./output/product_id.ttl"), RDFFormat.TURTLE);
+        Rio.write(companies_id, new FileWriter("./output/company_id.ttl"), RDFFormat.TURTLE);
         Rio.write(classes_id, new FileWriter("./output/product_type_id.ttl"), RDFFormat.TURTLE);
+
+        //
+        // dirLocations.txt to RDF
+        //
+
+        var locations = readDicLocations(TagRankingGenerator.class.getResourceAsStream("/dicLocations.txt"));
+
+        var builder = new ModelBuilder();
+
+        locations.entrySet().forEach(pair -> {
+            builder.subject("http://dbpedia.org/resource/" + pair.getKey()).add("http://schema.org/identifier", pair.getValue());
+        });
+
+        Rio.write(builder.build(), new FileWriter("./output/location_id.ttl"), RDFFormat.TURTLE);
     }
 
     private static Model assignIds(Set<String> set) {
+        // get consistent result
+        var sorted = new ArrayList<>(set);
+        sorted.sort(Comparator.naturalOrder());
+
         var builder = new ModelBuilder();
 
         int id = 0;
-        for (var s : set) {
+        for (var s : sorted) {
             builder.subject(s).add("http://schema.org/identifier", id++);
         }
 
         return builder.build();
+    }
+
+    private static Map<String, Integer> readDicLocations(InputStream resource) throws IOException {
+        var map = new HashMap<String, Integer>();
+
+        var br = new BufferedReader(new InputStreamReader(resource));
+
+        String line;
+        int id = 0;
+
+        while ((line = br.readLine()) != null)
+            map.put(line.split(" ")[1], id++);
+
+        return map;
     }
 }
