3df32cb9103a50219914d1ddc8948ebe0e6719b3
Paddy Xu <xupaddy@gmail.com>
2020-06-11 12:36:29 +0200

    move package downloads to build-time

--- gen.sh
+++ gen.sh
@@ -5,9 +5,9 @@ echo "Generating LDBC datasets..."
 sleep 5
 
 cd /code/ldbc_snb_datagen
-rm -rf parameter_curation
+rm -rf parameter_curation/*
 
-mvn compile exec:java -Dexec.mainClass="ldbc.snb.datagen.generator.LDBCDatagen" -Dexec.args="ldbc_params.ini"
+mvn exec:java -Dexec.mainClass=ldbc.snb.datagen.generator.LDBCDatagen -Dexec.args="ldbc_params.ini"
 
 
 echo
@@ -15,7 +15,7 @@ echo "Generating UniBench datasets..."
 sleep 5
 
 cd /code/UniBench
-rm -rf Unibench
+rm -rf Unibench/*
 
 sbt "runMain Unibench1_0"
 
